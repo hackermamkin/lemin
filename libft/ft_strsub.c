6e62a2a19ae86d83f6ee4c9ca7ca31e9912284d0
Gatharian Freddie <gfreddie@oa-b4.21-school.ru>
2020-03-01 14:51:13 +0300

    lol

---
+++ libft/ft_strsub.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gfreddie <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/09/11 14:47:10 by gfreddie          #+#    #+#             */
+/*   Updated: 2019/09/14 16:58:15 by gfreddie         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strsub(char const *s, unsigned int start, size_t len)
+{
+	char			*str;
+	unsigned int	i;
+
+	if (s)
+	{
+		if (!(str = (char *)malloc(sizeof(				char) * (len + 1))))
+			return (NULL);
+		i = 0;
+		while (s[i] && i < len)
+		{
+			str[i] = s[start + i];
+			i++;
+		}
+		str[i] = '\0';
+		return (str);
+	}
+	return (NULL);
+}
